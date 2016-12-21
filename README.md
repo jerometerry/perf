f52acbcd3c733356d2bcfbff0606d679ec42b87d
Jerome Terry <jerome.terry@verafin.com>
2016-12-21 00:11:56 -0330

    removing sudo from every command in generate-flamegraph.sh. instead, just sudo the entire file

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ git clone https://github.com/jerometerry/perf.git
 cd ./perf
 
 vagrant up
+
+vagrant ssh
+
+sudo ~/generate-flamegraph.sh
+
+exit
--- generate-flamegraph.sh
+++ generate-flamegraph.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sudo perf record -F 99 -a -g -- sleep 30
-sudo ./FlameGraph/jmaps
-sudo chown root /tmp/perf-*.map
-sudo chown root perf.data
-sudo perf script | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl --color=java --hash > flamegraph.svg
-sudo rm /tmp/perf-*.map
-sudo rm perf.data
+perf record -F 99 -a -g -- sleep 30
+./FlameGraph/jmaps
+chown root /tmp/perf-*.map
+chown root perf.data
+perf script | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl --color=java --hash > flamegraph.svg
+rm /tmp/perf-*.map
+rm perf.data
 cp ./flamegraph.svg /vagrant
-sudo rm ./flamegraph.svg
+rm ./flamegraph.svg
